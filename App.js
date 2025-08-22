768ce81b7cfe44ae6377d7dbbf1f7b6c08f8fc9e
Keerthana Rathinasamy <keerthirathinam01@gmail.com>
2025-08-22 12:04:06 +0530

    Contents changed

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const nestedChilds = React.createElement("div",{id:"child"},
     [React.createElement("div",{id:"first"},
-        [React.createElement("h1",{},"Keerthana"), React.createElement("h2",{},"Sibiling node")]), 
+        [React.createElement("h1",{},"First div"), React.createElement("h2",{},"Sibiling node")]), 
         React.createElement("div",{id:"second"},
         [React.createElement("h1",{},"second div"), React.createElement("h2",{},"Second Sibiling node")])
     ])
